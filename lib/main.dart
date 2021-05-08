f20d30d3ac1a9cd280ec370b98303d03e3ed65dd
luanguiming <18734184215@163.com>
2021-05-08 13:21:17 +0800

    flutter 基础demo完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_base_demo/app_lifecycle.dart';
+import 'package:flutter_base_demo/flutter_layout_page.dart';
+import 'package:flutter_base_demo/flutter_widget_lifecycle.dart';
+import 'package:flutter_base_demo/gesture_page.dart';
+import 'package:flutter_base_demo/launch_page.dart';
+import 'package:flutter_base_demo/less_group_page.dart';
+import 'package:flutter_base_demo/photo_app_page.dart';
+import 'package:flutter_base_demo/plugin_use.dart';
+import 'package:flutter_base_demo/res_page.dart';
+import 'package:flutter_base_demo/statefull_group_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(DynamicTheme());
+
+class DynamicTheme extends StatefulWidget {
+  @override
+  _DynamicThemeState createState() => _DynamicThemeState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _DynamicThemeState extends State<DynamicTheme> {
+  Brightness _brightness = Brightness.light;
+  ///字体下载地址：https://fonts.google.com/specimen/Rubik+Mono+One?selection.family=Rubik+Mono+One
+  ///更多参考：https://flutter.dev/docs/cookbook/design/fonts
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+//          fontFamily: 'RubikMonoOne',//将该字体应用到全局
+          brightness:_brightness,
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('如何创建和使用Flutter的路由与导航？'),
+          ),
+          body: Column(
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    if (_brightness == Brightness.dark) {
+                      _brightness = Brightness.light;
+                    } else {
+                      _brightness = Brightness.dark;
+                    }
+                  });
+                },
+                child: Text('切换主题abc',style: TextStyle(fontFamily: 'RubikMonoOne'),),
+              ),
+              RouteNavigator()
+            ],
+          ),
+        ),
+        routes: <String, WidgetBuilder>{
+          'plugin': (BuildContext context) => PluginUse(),
+          'less': (BuildContext context) => LessGroupPage(),
+          'ful': (BuildContext context) => StatefulGroup(),
+          'layout': (BuildContext context) => FlutterLayoutPage(),
+          'gesture': (BuildContext context) => GesturePage(),
+          'res': (BuildContext context) => ResPage(),
+          'launch': (BuildContext context) => LaunchPage(),
+          'widgetLifecycle': (BuildContext context) => WidgetLifecycle(),
+          'appLifecycle': (BuildContext context) => AppLifecycle(),
+          'photo': (BuildContext context) => PhotoApp(),
+        });
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class RouteNavigator extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _RouteNavigatorState createState() => _RouteNavigatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _RouteNavigatorState extends State<RouteNavigator> {
+  bool byName = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      child: Column(
+        children: <Widget>[
+          SwitchListTile(
+              title: Text('${byName ? '' : '不'}通过路由名跳转'),
+              value: byName,
+              onChanged: (value) {
+                setState(() {
+                  byName = value;
+                });
+              }),
+          _item('如何使用Flutter包和插件？', PluginUse(), 'plugin'),
+          _item('StatelessWidget与基础组件', LessGroupPage(), 'less'),
+          _item('StatefulWidget与基组件', StatefulGroup(), 'ful'),
+          _item('如何进行Flutter布局开发', FlutterLayoutPage(), 'layout'),
+          _item('如何检测用户手势以及处理点击事件？', GesturePage(), 'gesture'),
+          _item('如何导入和使用Flutter的资源文件？', ResPage(), 'res'),
+          _item('如何打开第三方应用？', LaunchPage(), 'launch'),
+          _item('Flutter页面生命周期', WidgetLifecycle(), 'widgetLifecycle'),
+          _item('Flutter应用生命周期', AppLifecycle(), 'appLifecycle'),
+          _item('【实战尝鲜】拍照APP开发', PhotoApp(), 'photo'),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+    );
+  }
+
+  _item(String title, page, String routeName) {
+    return Container(
+      child: RaisedButton(
+        onPressed: () {
+          if (byName) {
+            Navigator.pushNamed(context, routeName);
+          } else {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => page));
+          }
+        },
+        child: Text(title),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
